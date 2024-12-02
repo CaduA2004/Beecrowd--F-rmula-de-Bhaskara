0a0e28b280d5704a4f0143e1e4f8329bab60978f
cadu <andradecadu14@gmail.com>
2024-12-02 18:07:32 -0300

    first commit

---
+++ formula-de-bhaskara.cs
@@ -0,0 +1,28 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        string[] input = Console.ReadLine().Split(' ');
+        double A = double.Parse(input[0]);
+        double B = double.Parse(input[1]);
+        double C = double.Parse(input[2]);
+
+        double delta = B * B - 4 * A * C;
+
+        if (A == 0 || delta < 0)
+        {
+            Console.WriteLine("Impossivel calcular");
+        }
+        else
+        {
+            double R1 = (-B + Math.Sqrt(delta)) / (2 * A);
+            double R2 = (-B - Math.Sqrt(delta)) / (2 * A);
+
+            Console.WriteLine("R1 = " + R1.ToString("F5"));
+            Console.WriteLine("R2 = " + R2.ToString("F5"));
+        }
+    }
+
+}
